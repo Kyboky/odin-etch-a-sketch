26e8a888562f9fe4330249fa8fa88196be9326a7
Boris Kosanovic <boksavg@gmail.com>
2022-01-31 23:37:15 +0100

    Added mouse controls

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,21 +1,45 @@
 const canvas = document.querySelector(".canvas");
+let isLeftMouseButtonPressed = false;
+let isRightMouseButtonPressed = false;
 
-
-function addPixelElements(numOfCols){
-    const numOfElements = numOfCols * numOfCols;
-    // const pixelWidthHeight = Math.round(canvas.clientWidth/numOfCols) - 1;
-    const pixelPercentage = 100/numOfCols;
+function mouseEvents(){
+    window.addEventListener('contextmenu', (event) => {
+        console.log(event.button);
+        event.preventDefault();
+    })
     
-    let pixelElement = document.createElement("div");
-    // pixelElement.style.width = `${pixelWidthHeight}px`;
-    // pixelElement.style.height = `${pixelWidthHeight}px`;
-    pixelElement.style.width = `${pixelPercentage}%`;
-    pixelElement.style.height = `${pixelPercentage}%`;
-    pixelElement.classList = "pixel";
-    // pixelElement.addEventListener("mouseover",changeBackgroundColor);
+    window.addEventListener('mousedown', (eventDown) => {
+        eventDown.preventDefault();
+        if (eventDown.button == 0){
+            isLeftMouseButtonPressed = true;
+        }
+        if (eventDown.button == 2){
+            isRightMouseButtonPressed = true;
+        }
+    })
+    
+    window.addEventListener('mouseup', (eventUp) => {
+        if (eventUp.button == 0){
+            isLeftMouseButtonPressed = false;
+        }
+        if (eventUp.button == 2){
+            isRightMouseButtonPressed = false;
+        }
+    })
+}
+
+function createGrid(numOfCols){
+    const numOfElements = numOfCols * numOfCols;
+    const pixelSizePercent = 100/numOfCols;
+    
+    let pixel = document.createElement("div");
+    pixel.style.width = `${pixelSizePercent}%`;
+    pixel.style.height = `${pixelSizePercent}%`;
+    pixel.classList = "pixel";
+    pixel.setAttribute('draggable', false);
 
     for(let i = 0; i < numOfElements; i++){
-        const clone = pixelElement.cloneNode(true);
+        const clone = pixel.cloneNode(true);
         clone.addEventListener("mouseover",changeBackgroundColor);
         canvas.appendChild(clone);
     }
@@ -25,12 +49,15 @@ function randomColor(){
     const r = Math.floor(Math.random() * 255.99);
     const g = Math.floor(Math.random() * 255.99);
     const b = Math.floor(Math.random() * 255.99);
-    return `rgb(${r},${g},${b})`
+    return `rgb(${r},${g},${b})`;
 }
 
 function changeBackgroundColor(){
-    console.log(this.style.backgroundColor);
-    this.style.backgroundColor = randomColor();
+    if (isLeftMouseButtonPressed){
+        this.style.backgroundColor = randomColor();
+    } else if (isRightMouseButtonPressed){
+        this.style.backgroundColor = "rgb(255, 255, 255)";
+    }   
 }
 
 function refreshCanvas(){
@@ -39,11 +66,8 @@ function refreshCanvas(){
     let fieldSize = parseInt(prompt("Enter the size of field (10-100)"));
     fieldSize = Math.min(fieldSize, 100);
     fieldSize = Math.max(fieldSize, 10);
-    addPixelElements(fieldSize);
+    createGrid(fieldSize);
 }
 
-addPixelElements(16);
-console.log(canvas.clientWidth/16);
-
-
-console.log("Hello world")
+mouseEvents();
+createGrid(16);
