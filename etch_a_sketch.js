af694b74f146c7e4fc31eb6daf0050c3864a1feb
Boris Kosanovic <boksavg@gmail.com>
2022-01-31 22:27:02 +0100

    Added colorfull canvas

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,1 +1,49 @@
+const canvas = document.querySelector(".canvas");
+
+
+function addPixelElements(numOfCols){
+    const numOfElements = numOfCols * numOfCols;
+    // const pixelWidthHeight = Math.round(canvas.clientWidth/numOfCols) - 1;
+    const pixelPercentage = 100/numOfCols;
+    
+    let pixelElement = document.createElement("div");
+    // pixelElement.style.width = `${pixelWidthHeight}px`;
+    // pixelElement.style.height = `${pixelWidthHeight}px`;
+    pixelElement.style.width = `${pixelPercentage}%`;
+    pixelElement.style.height = `${pixelPercentage}%`;
+    pixelElement.classList = "pixel";
+    // pixelElement.addEventListener("mouseover",changeBackgroundColor);
+
+    for(let i = 0; i < numOfElements; i++){
+        const clone = pixelElement.cloneNode(true);
+        clone.addEventListener("mouseover",changeBackgroundColor);
+        canvas.appendChild(clone);
+    }
+}
+
+function randomColor(){
+    const r = Math.floor(Math.random() * 255.99);
+    const g = Math.floor(Math.random() * 255.99);
+    const b = Math.floor(Math.random() * 255.99);
+    return `rgb(${r},${g},${b})`
+}
+
+function changeBackgroundColor(){
+    console.log(this.style.backgroundColor);
+    this.style.backgroundColor = randomColor();
+}
+
+function refreshCanvas(){
+    console.log("refresh");
+    canvas.innerHTML = "";
+    let fieldSize = parseInt(prompt("Enter the size of field (10-100)"));
+    fieldSize = Math.min(fieldSize, 100);
+    fieldSize = Math.max(fieldSize, 10);
+    addPixelElements(fieldSize);
+}
+
+addPixelElements(16);
+console.log(canvas.clientWidth/16);
+
+
 console.log("Hello world")
